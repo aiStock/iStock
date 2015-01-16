3360fb5308fdfd2d5506eb535b15c2d07ed3c973
Mif <569576618@qq.com>
2015-01-17 01:35:02 +0800

    up

---
+++ hello.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: changwenguang
+ * Date: 15/1/17
+ * Time: 上午1:20
+ */
+
+class hello {
+
+
+    private $username;
+
+    private $hello;
+
+    private $master;
+
+    /**
+     * @this is a hello document
+     */
+    public function helloo(){
+
+
+
+
+    }
+
+}
